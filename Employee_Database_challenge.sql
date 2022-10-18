6062cedda9360817262d8d7cae729dc7976bdecb
AadiJan <112893464+AadiJan@users.noreply.github.com>
2022-10-18 00:08:36 -0400

    Add files via upload

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -83,3 +83,14 @@ SELECT COUNT(emp_no), title
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(emp_no) DESC;
+
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no, employees.first_name, employees.last_name, employees.birth_date, dept_emp.from_date, dept_emp.to_date, titles.title
+INTO mentorship_eligibilty 
+FROM employees
+INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+INNER JOIN titles ON employees.emp_no = titles.emp_no
+WHERE (dept_emp.to_date = '9999-01-01') AND (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+SELECT * FROM mentorship_eligibilty;
+DROP TABLE mentorship_eligibilty;
